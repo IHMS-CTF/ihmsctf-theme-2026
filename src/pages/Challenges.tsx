61365e0a1514606e6e877a044aee41c17a05d37b
milesmuehlbach <miles@flightsuitsystems.com>
2026-03-02 08:18:05 -0600

    small visual tweak x2

--- src/pages/Challenges.tsx
+++ src/pages/Challenges.tsx
@@ -127,7 +127,7 @@ const Challenges: React.FC<ChallengesProps> = () => {
                   <div className="header-line"></div>
                 </div>
 
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-1 gap-2">
+                <div className="flex flex-col gap-2">
                   {categoryChallenges.map((challenge: any) => (
                     <button
                       key={challenge.id}
